5e632530c85ce1ca8c6e35708271f9f7664a1cd6
Sofrosine <soultanmuh@gmail.com>
2021-07-29 19:27:17 +0700

    add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
-import 'tailwindcss/tailwind.css';
-import '../global.css';
+import "tailwindcss/tailwind.css";
+import "../global.css";
+import Router from "next/router";
+import Head from "next/head";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
-function MyApp({Component, pageProps}) {
-  return <Component {...pageProps} />;
+Router.events.on("routeChangeStart", (url) => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
+function MyApp({ Component, router, pageProps }) {
+  return <Component key={router.route} {...pageProps} />;
 }
 
 export default MyApp;
