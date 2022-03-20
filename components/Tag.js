cf51e74a17b5c2cca5a6fcdff41900047d4de305
Sofrosine <soultanmuh@gmail.com>
2022-03-20 21:31:34 +0700

    tag

--- components/Tag.js
+++ components/Tag.js
@@ -2,7 +2,14 @@ import { capitalizeEachWords } from "../utils";
 
 const Tag = ({ item }) => {
   return (
-    <div className={item?.bgColor}>
+    <div
+      className={
+        "shadow-md py-1 px-2 rounded-sm " +
+        (item?.darkBgColor ? "dark:" + item?.darkBgColor : "") +
+        " " +
+        item?.bgColor
+      }
+    >
       <p className="text-xs">{capitalizeEachWords(item?.name || "")}</p>
     </div>
   );
