5932b8949e017185d72155f18fc71d35977f804e
Sofrosine <soultanmuh@gmail.com>
2022-03-20 21:16:55 +0700

    tag again

--- components/Tag.js
+++ components/Tag.js
@@ -4,10 +4,7 @@ const Tag = ({ item }) => {
   return (
     <div
       className={
-        "shadow-md py-1 px-2 rounded-sm " +
-        (item?.darkBgColor ? "dark:" + item?.darkBgColor : "") +
-        " " +
-        item?.bgColor
+        "shadow-md py-1 px-2 rounded-sm bg-orange dark:bg-red"
       }
     >
       <p className="text-xs">{capitalizeEachWords(item?.name || "")}</p>
