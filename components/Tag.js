6833b48b13a14da4d4a95db255ae7504a99621cb
Sofrosine <soultanmuh@gmail.com>
2022-03-20 20:39:49 +0700

    tag

--- components/Tag.js
+++ components/Tag.js
@@ -1,26 +1,21 @@
-import { useEffect, useState } from "react";
 import { capitalizeEachWords } from "../utils";
 
 const Tag = ({ item }) => {
-  const [mounted, setMounted] = useState(false);
-
-  // When mounted on client, now we can show the UI
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
   return (
     <div
       className={
         "shadow-md py-1 px-2 rounded-sm " +
-        "dark:" +
-        item?.darkBgColor +
+        (item?.darkBgColor ? "dark:" + item?.darkBgColor : "") +
         " " +
         item?.bgColor
       }
     >
-      <p className="text-xs">{capitalizeEachWords(item?.name || "")}</p>
+      <p className="text-xs">
+        {capitalizeEachWords(item?.name || "") +
+          (item?.darkBgColor ? " dark:" + item?.darkBgColor : "") +
+          " " +
+          item?.bgColor}
+      </p>
     </div>
   );
 };
