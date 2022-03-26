66e9d0e7f30f5999776872daff0a5a414773f5fc
Sofrosine <soultanmuh@gmail.com>
2022-03-26 11:37:24 +0700

    test

--- components/Tag.js
+++ components/Tag.js
@@ -1,15 +1,16 @@
+import { useEffect } from "react";
 import { capitalizeEachWords } from "../utils";
 
 const Tag = ({ item }) => {
+  var classes = ["shadow-md", "py-1", "px-2", "rounded-sm"];
+
+  if (item?.darkBgColor !== "") {
+    classes.push(`dark:${item?.darkBgColor}`);
+  }
+  classes.push(item?.bgColor);
+
   return (
-    <div
-      className={`shadow-md py-1 px-2 rounded-sm 
-        ${
-          (item?.darkBgColor !== "" ? "dark:" + item?.darkBgColor : "") +
-          " " +
-          item?.bgColor
-        }`}
-    >
+    <div className={classes.join(" ")}>
       <p className="text-xs">{capitalizeEachWords(item?.name || "")}</p>
     </div>
   );
